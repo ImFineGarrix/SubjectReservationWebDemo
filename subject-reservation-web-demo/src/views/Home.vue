0b78f09a6bd0de8ff89cc8e829c6576f9cdccceb
admin <admin@admins-MacBook-Pro.local>
2021-08-07 11:53:31 +0700

    update version Home.vue

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web as P
+      SIT Subject Reservation Web as kuy
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
